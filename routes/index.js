efe4b362efa9136f7dc6f5203f6bd796103d9926
arianaolson419 <ariana.olson@students.olin.edu>
2017-03-28 21:18:00 -0400

    require majorModel for testing and further use

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const Major = require('../models/majorModel');
+
 //returns the home page html, index.html
 router.get('/', function(req, res, next) {
 	res.render('index', {});
