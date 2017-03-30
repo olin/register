a0b72d413e548998f8ad10c6d96bb4fd0cb2aae8
arianaolson419 <ariana.olson@students.olin.edu>
2017-03-30 10:44:28 -0400

    rendering StudentHome component for testing

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import App from './components/App';
+// import App from './components/App';
+import StudentHome from './components/StudentHome';
 import reducer from './reducers/sampleReducer';
 
 const store = createStore(reducer);
 
 render(
   <Provider store={store}>
-    <App />
+    <StudentHome />
   </Provider>,
   document.getElementById('container'),
 );
