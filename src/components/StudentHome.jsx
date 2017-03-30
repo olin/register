5537a34778588c1b0c12e3112e6ad84d172aa69a
arianaolson419 <ariana.olson@students.olin.edu>
2017-03-30 17:46:34 -0400

    Integrates Plan of Study into component

--- src/components/StudentHome.jsx
+++ src/components/StudentHome.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import WelcomeMessage from './WelcomeMessage';
+import PlanOfStudy from './PlanOfStudy';
 
 // This will be the wrapper for all components of a studen home page
 const StudentHome = () => (
   <div>
     <h2>
-      StudentHome
+      Student Home
     </h2>
     <WelcomeMessage student="Ariana Olson" />
+    <PlanOfStudy />
   </div>
 );
 
