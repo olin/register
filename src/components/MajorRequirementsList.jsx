90561a06b538a69d61a7de59da6b8bb5d9e18a3e
arianaolson419 <ariana.olson@students.olin.edu>
2017-03-30 17:05:08 -0400

    Adds component list of all major requirements

---
+++ src/components/MajorRequirementsList.jsx
@@ -0,0 +1,21 @@
+import React, { PropTypes } from 'react';
+import MajorRequirement from './MajorRequirement';
+
+// A list containing all of the Major requirements a
+// student needs to graduate
+const MajorRequirementsList = ({ requirements }) => (
+  <ul>
+    {requirements.map(req =>
+      <MajorRequirement
+        key={req.toString()}
+        requirement={req}
+      />
+    )}
+  </ul>
+);
+
+MajorRequirementsList.propTypes = {
+  requirements: PropTypes.array.isRequired,
+};
+
+export default MajorRequirementsList;
