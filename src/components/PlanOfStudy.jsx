4b2a7fb508ed7fb0f5c705d896550d83e0f30393
arianaolson419 <ariana.olson@students.olin.edu>
2017-03-30 17:45:05 -0400

    Integrates requirements lists into POS

--- src/components/PlanOfStudy.jsx
+++ src/components/PlanOfStudy.jsx
@@ -1,18 +1,47 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import GeneralRequirementsList from './GeneralRequirementsList';
+import MajorRequirementsList from './MajorRequirementsList';
 
 // This component is for displaying a student's plan of study.
-// It will have a block of general requirements and a block of 
-// major-specific requirements 
+// It will have a block of general requirements and a block of
+// major-specific requirements
 const PlanOfStudy = () => (
   <div>
     <p>Plan of Study</p>
-    {/* This will contain major and general 
-    requirements once those components are made*/}
+    <p>General Requirements</p>
+    <GeneralRequirementsList
+      requirements={[
+        'Modeling and Simulation of the Physical World',
+        'Linearity I',
+        'Linearity II',
+        'Probability and Statistics',
+        'Biology Foundation',
+        'Chemistry/Materials Science',
+        'Physics Foundation',
+        'Introduction to Sensors, Instrumentation, and Measurement',
+        'Principles of Engineering',
+        'Engineering Capstone',
+        'Design Nature',
+        'User-Oriented Collaborative Design',
+        'Design Depth',
+        'AHS Foundation',
+        'AHS or Entrepreneurship Depth of Study',
+        'Olin Self Study',
+      ]}
+    />
+    <p>Major Requirements</p>
+    <MajorRequirementsList
+      requirements={[
+        'Mechanical Engineering Math',
+        'Mechanics of Solids and Structures',
+        'Dynamics',
+        'Thermodynamics',
+        'Transport Phenomena',
+        'Mechanical Design',
+        'Mechanical Engineering Elective',
+      ]}
+    />
   </div>
 );
 
-WelcomeMessage.propTypes = {
-  student: PropTypes.string.isRequired,
-};
-
-export default WelcomeMessage;
+export default PlanOfStudy;
