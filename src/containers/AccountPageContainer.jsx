b3a76a380f47ef3a8d04e5a48ecacb606468391e
Halliax <tmsheneman@gmail.com>
2017-03-31 00:54:34 -0400

    moved AccountPage component to components/

--- src/containers/AccountPageContainer.jsx
+++ src/containers/AccountPageContainer.jsx
@@ -1,28 +1,6 @@
-import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { toggleSetting } from '../actions/actions';
-import Setting from '../components/Setting';
-
-const AccountPage = ({ settings, onSettingToggle }) => (
-  <ul>
-    {settings.map(setting =>
-      <Setting
-        name={setting.name}
-        key={setting.name}
-        {...setting}
-        onClick={() => onSettingToggle(setting.name)}
-      />,
-    )}
-  </ul>
-);
-
-AccountPage.propTypes = {
-  settings: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    checked: PropTypes.bool.isRequired,
-  }).isRequired).isRequired,
-  onSettingToggle: PropTypes.func.isRequired,
-};
+import AccountPage from '../components/AccountPage';
 
 // Connect with Presentational Component
 const mapStateToProps = state => ({
