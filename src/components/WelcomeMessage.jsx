e42d63433f265e52af00849fccd95baf7760accf
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-03 23:05:39 -0400

    Change name of prop to reflect user object key

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -2,16 +2,16 @@ import React, { PropTypes } from 'react';
 
 // This component renders a welcome message to the logged in student.
 // It does not yet take into account if no one is logged in
-const WelcomeMessage = ({ student }) => (
+const WelcomeMessage = ({ name }) => (
   <div>
     <p>
-      Welcome, {student}
+      Welcome, {name}
     </p>
   </div>
 );
 
 WelcomeMessage.propTypes = {
-  student: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 export default WelcomeMessage;
