f4a20396b6b1eaa6186d7e0467732fda00d6a621
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-03 23:01:51 -0400

    Add user prop to component
    Because there is now a comtainer for this component, it now needs to
    be able to accept a user object a a property. Elements of that object
    are passed to children as needed.

--- src/components/StudentHome.jsx
+++ src/components/StudentHome.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import WelcomeMessage from './WelcomeMessage';
 import PlanOfStudy from './PlanOfStudy';
-
 // The wrapper for all components on a student home page
-const StudentHome = () => (
+const StudentHome = ({ user }) => (
   <div>
     <h2>
       Student Home
     </h2>
-    <WelcomeMessage student="Ariana Olson" />
+    <WelcomeMessage name={user.name} />
     <PlanOfStudy />
   </div>
 );
+
+StudentHome.propTypes = {
+  user: PropTypes.object.shape({
+    email: PropTypes.string,
+    name: PropTypes.string,
+    olinId: PropTypes.string,
+    entryYear: PropTypes.number,
+    major: PropTypes.string,
+    completedCourses: PropTypes.arrayOf(PropTypes.string),
+    plannedCourses: PropTypes.arrayOf(PropTypes.string),
+    _id: PropTypes.string,
+  }).isRequired,
+};
 
 export default StudentHome;
