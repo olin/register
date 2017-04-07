57d29d0bb88075553258bdc44005faa4b8898969
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-07 16:26:01 -0400

    Add actions for login form updates
    These are taken from the redux tutorial code in class. Part of an effort
    to reduxify the login page.

--- src/actions/actions.jsx
+++ src/actions/actions.jsx
@@ -2,3 +2,13 @@ export const toggleSetting = name => ({
 	type: 'TOGGLE_SETTING',
 	name,
 });
+
+export const updatePassword = password => ({
+	type: 'UPDATE_PASSWORD',
+	password,
+});
+
+export const updateUsername = username => ({
+	type: 'UPDATE_USERNAME',
+	username,
+});
