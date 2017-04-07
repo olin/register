9cb11ed54077ed7f9819a555e8d166f238353f4c
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-07 16:54:12 -0400

    Convert component to presentational component

--- src/components/LoginLocal.jsx
+++ src/components/LoginLocal.jsx
@@ -1,65 +1,38 @@
-import React from 'react';
-import $ from 'jquery';
+import React, { PropTypes } from 'react';
 
-export default class LoginLocal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-    };
-    this.updateNewUser = this.updateNewUser.bind(this);
-    this.updatePassword = this.updatePassword.bind(this);
-    this.login = this.login.bind(this);
-  }
-  updateNewUser(e) {
-    this.setState({
-      username: e.target.value,
-    });
-  }
-  updatePassword(e) {
-    this.setState({
-      password: e.target.value,
-    });
-  }
-  login(e) {
-    e.preventDefault();
-    const formData = {
-      username: this.state.username,
-      password: this.state.password,
-    };
-    $.post('/login', formData)
-      .error((err, status) => {
-        console.error(status);
-      });
-  }
-  render() {
-    return (
-      <div>
-        <form id="local-login" onSubmit={this.login} className="form-signin">
-          <h2>Login with email and password</h2>
-          <input
-            placeholder="Email"
-            type="text"
-            value={this.state.username}
-            onChange={this.updateNewUser}
-          />
-          <br />
-          <input
-            placeholder="Password"
-            type="password"
-            value={this.state.password}
-            onChange={this.updatePassword}
-          />
-          <br />
-          <button
-            type="submit"
-            value="Login"
-          >
-          Login
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+const LoginLocal = ({ username, password, updateUser, updatePassword }) => (
+  <div>
+    <form id="local-login" classname="form-signin">
+      <h2>Login with email and password</h2>
+      <input
+        placeholder="Email"
+        type="text"
+        value={username}
+        onChange={updateNewUser}
+      />
+      <br />
+      <input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={updatePassword}
+      />
+      <br />
+      <button
+        type="submit"
+        value="Login"
+      >
+      Login
+      </button>
+    </form>
+  </div>
+);
+
+LoginLocal.propTypes = {
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  updateNewUser: PropTypes.func.isRequired,
+  updatePassword: propTypes.func.isRequired,
+};
+
+export default LoginLocal;
