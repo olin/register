04ea3a907db45912f0a8f33223b937df67678e20
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-07 21:42:34 -0400

    Add Register actions

--- src/actions/actions.jsx
+++ src/actions/actions.jsx
@@ -1,8 +1,10 @@
+// Account Page Component
 export const toggleSetting = name => ({
 	type: 'TOGGLE_SETTING',
 	name,
 });
 
+// Login Component
 export const updatePassword = password => ({
 	type: 'UPDATE_PASSWORD',
 	password,
@@ -11,4 +13,20 @@ export const updatePassword = password => ({
 export const updateUsername = username => ({
 	type: 'UPDATE_USERNAME',
 	username,
+});
+
+// Register Component
+export const updateNewUsername = username => ({
+	type: 'UPDATE_NEW_USERNAME',
+	username,
+});
+
+export const updateNewPassword = password => ({
+	type: 'UPDATE_NEW_PASSWORD',
+	password,
+});
+
+export const confirmPassword = password => ({
+	type: 'CONFIRM_PASSWORD',
+	password,
 });
