7ef5ea89353eaff0af36cfe78b678edcc15e1cb2
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-07 22:15:06 -0400

    Create Register contatiner component

---
+++ src/containers/RegisterContainer.jsx
@@ -0,0 +1,25 @@
+import { connect } from 'react-redux';
+import { updateNewUsername, updateNewPassword, confirmPassword } from '../actions/actions'
+import RegisterNewUser from '../components/RegisterNewUser';
+
+const mapStateToProps = state => ({
+  username: state.username,
+  password: state.password,
+  confirmPassword: state.confirmPassword,
+});
+
+const mapDispatchToProps = dispatch => ({
+  updateNewUsername: (e) => {
+    dispatch(updateNewUsername(e.target.value));
+  },
+  updateNewPassword: (e) => {
+    dispatch(updateNewPassword(e.target.value));
+  },
+});
+
+const RegisterContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(RegisterNewUser);
+
+export default RegisterNewUser;
