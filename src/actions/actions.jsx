32e8ab39ffcee7b7a8fa5eb02460208d60234972
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-09 11:52:08 -0400

    Add actions for login backend interaction

--- src/actions/actions.jsx
+++ src/actions/actions.jsx
@@ -1,32 +1,47 @@
 // Account Page Component
 export const toggleSetting = name => ({
-	type: 'TOGGLE_SETTING',
-	name,
+  type: 'TOGGLE_SETTING',
+  name,
 });
 
 // Login Component
 export const updatePassword = password => ({
-	type: 'UPDATE_PASSWORD',
-	password,
+  type: 'UPDATE_PASSWORD',
+  password,
 });
 
 export const updateUsername = username => ({
-	type: 'UPDATE_USERNAME',
-	username,
+  type: 'UPDATE_USERNAME',
+  username,
 });
 
 // Register Component
 export const updateNewUsername = username => ({
-	type: 'UPDATE_NEW_USERNAME',
-	username,
+  type: 'UPDATE_NEW_USERNAME',
+  username,
 });
 
 export const updateNewPassword = password => ({
-	type: 'UPDATE_NEW_PASSWORD',
-	password,
+  type: 'UPDATE_NEW_PASSWORD',
+  password,
 });
 
 export const updateConfirmPassword = password => ({
-	type: 'UPDATE_CONFIRM_PASSWORD',
-	password,
+  type: 'UPDATE_CONFIRM_PASSWORD',
+  password,
 });
+
+// Login backend interaction
+export const userLogin = formData => ({
+  type: 'USER_LOGIN',
+  // form data consists of email and password
+  formData,
+});
+
+export const receiveUser = json => ({
+  type: 'RECEIVE_USER',
+  username: json.username,
+  plannedCourses: json.plannedCourses,
+  completedCourses: json.completedCourses,
+});
+
