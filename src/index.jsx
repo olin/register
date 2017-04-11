6eabc6a06905fe441f53e91b15d2c10bc517d0ff
Halliax <tmsheneman@gmail.com>
2017-04-11 13:59:32 -0400

    allow redux-dev-tools chrome plugin to access store

--- src/index.jsx
+++ src/index.jsx
@@ -6,11 +6,12 @@ import { Provider } from 'react-redux';
 import Login from './components/Login';
 import reducer from './reducers/AccountPageReducer';
 
-
+/* eslint-disable no-underscore-dangle */
 const store = createStore(
   reducer,
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
+/* eslint-enable */
 
 render(
   <Provider store={store}>
