83e08225f878023007339cf66aabd8c5651a9168
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-12 00:57:15 -0400

    Change login after register code
    The passport.authenticate function did not work the way it was being
    implemented in the /register route. By using req.login instead,
    the user is successfully logged in after registering.

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,26 @@ const express = require('express');
 const passport = require('passport');
 const Student = require('./../models/studentModel');
 
+const ensureAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  return res.redirect('/login');
+};
 
 const router = express.Router();
 
-// returns the home page html, index.html
 router.get('/', (req, res) => {
   res.render('index', {});
 });
 
+router.get('/user', (req) => {
+  console.log(req.user);
+});
 // log in with local strategy
 router.post('/login', passport.authenticate('local'),
   (req, res) => {
+    console.log('Clientside post');
     res.json({
       username: req.user.username,
       id: req.user.id,
@@ -20,18 +29,26 @@ router.post('/login', passport.authenticate('local'),
   });
 
 // register new user
-router.post('/register', (req) => {
+router.post('/register', (req, res) => {
   Student.register(new Student({ username: req.body.username }),
     req.body.password, (err, account) => {
       if (err) {
         console.error(err);
       }
-
-      passport.authenticate('login', (res) => {
-        res.json({
-          username: account.username,
-          id: account.id,
-        });
+      account.save((err) => {
+        if(err) {
+          console.log(err);
+        } else {
+          req.login(account, (err) => {
+            if(err) {
+              console.log(err);
+            };
+            res.json({
+              username: req.user.username,
+              id: req.user.id,
+            })
+          })
+        }
       });
     });
 });
