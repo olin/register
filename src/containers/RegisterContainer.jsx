f463dfebfddda76afc444846ac64d3b41ff31b54
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-12 01:05:51 -0400

    Incorporate register action and switch to mergeProps
    In order to access the current state for registration purposes, it was
    necessary to use mergeProps as an argument to the connect
    fuction.

--- src/containers/RegisterContainer.jsx
+++ src/containers/RegisterContainer.jsx
@@ -1,28 +1,52 @@
 import { connect } from 'react-redux';
-import { updateNewUsername, updateNewPassword, updateConfirmPassword } from '../actions/actions';
+import { updateNewUsername, updateNewPassword, updateConfirmPassword, register } from '../actions/actions';
 import RegisterNewUser from '../components/RegisterNewUser';
 
 const mapStateToProps = state => ({
-  username: state.username,
-  password: state.password,
-  confirmPassword: state.confirmPassword,
+  username: state.RegisterReducer.username,
+  password: state.RegisterReducer.password,
+  confirmPassword: state.RegisterReducer.confirmPassword,
 });
 
-const mapDispatchToProps = dispatch => ({
-  updateNewUsername: (e) => {
+// const mapDispatchToProps = dispatch => ({
+//   updateNewUsername: (e) => {
+//     dispatch(updateNewUsername(e.target.value));
+//   },
+//   updateNewPassword: (e) => {
+//     dispatch(updateNewPassword(e.target.value));
+//   },
+//   updateConfirmPassword: (e) => {
+//     dispatch(updateConfirmPassword(e.target.value));
+//   },
+// });
+
+const mergeProps = (stateProps, dispatchProps, ownProps) => {
+  const { username, password, confirmPassword } = stateProps;
+  const { dispatch } = dispatchProps;
+
+  return {
+    updateNewUsername: (e) => {
     dispatch(updateNewUsername(e.target.value));
-  },
-  updateNewPassword: (e) => {
-    dispatch(updateNewPassword(e.target.value));
-  },
-  updateConfirmPassword: (e) => {
-    dispatch(updateConfirmPassword(e.target.value));
-  },
-});
+    },
+    updateNewPassword: (e) => {
+      dispatch(updateNewPassword(e.target.value));
+    },
+    updateConfirmPassword: (e) => {
+      dispatch(updateConfirmPassword(e.target.value));
+    },   
+    onRegister: (e) => {
+      e.preventDefault();
+      console.log(username, password, 'register');
+      dispatch(register(username, password));
+    },
+  };
+
+}
 
 const RegisterContainer = connect(
   mapStateToProps,
-  mapDispatchToProps,
+  null,
+  mergeProps,
 )(RegisterNewUser);
 
 export default RegisterContainer;
