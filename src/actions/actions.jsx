b78b831f1c3d746601d6b3fa7150849ea9dbf967
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-13 09:14:03 -0400

    Add actions for getting grad requirements and courses by specific requirement

--- src/actions/actions.jsx
+++ src/actions/actions.jsx
@@ -1,5 +1,40 @@
-export function toggleSetting(name) {
-  return { type: 'TOGGLE_SETTING', name };
-}
+import $ from 'jquery';
 
-export default toggleSetting;
+export const toggleSetting = (name) => ({
+	type: 'TOGGLE_SETTING',
+	name,
+});
+
+// Graduation requirements by major
+
+export const receiveRequirements = (json) =>({
+	type: 'RECEIVE_REQUIREMENTS',
+	generalRequirements: json.generalRequirements,
+	majorRequirements: json.majorRequirements,
+});
+
+export const postRequirements = (major) => (
+	(dispatch) => {
+		data = { major };
+		$.post('/requirements', data)
+			.done((json) => dispatch(receiveRequirements(json)))
+			.fail((err, status) => console.error(err, status));
+	};
+);
+
+// Courses by requirements
+
+export const receiveCoursesByReq = (requirement, json) => ({
+	type: 'RECEIVE_COURSES_BY_REQ',
+	requirement,
+	courses: json.courses,
+});
+
+export const postCoursesByReq = (requirement) => (
+	(dispatch) => {
+		data = { requirement };
+		$.post('/coursesbyreq', data)
+			.done((json) => dispatch(receiveRequirements(requirement, json)))
+			.fail((err, status) => console.error(err, status));
+	};
+);
