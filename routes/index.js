9ea44f6ccfa29795142b21c824662b0ebe614c13
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-12 22:33:50 -0400

    Transfer router fixes from REG-20

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const Student = require('./../models/studentModel');
+const path = require('path');
 
 const ensureAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -11,17 +12,9 @@ const ensureAuthenticated = (req, res, next) => {
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index', {});
-});
-
-router.get('/user', (req) => {
-  console.log(req.user);
-});
 // log in with local strategy
 router.post('/login', passport.authenticate('local'),
   (req, res) => {
-    console.log('Clientside post');
     res.json({
       username: req.user.username,
       id: req.user.id,
@@ -31,26 +24,36 @@ router.post('/login', passport.authenticate('local'),
 // register new user
 router.post('/register', (req, res) => {
   Student.register(new Student({ username: req.body.username }),
-    req.body.password, (err, account) => {
-      if (err) {
-        console.error(err);
+    req.body.password, (regErr, account) => {
+      if (regErr) {
+        console.error(regErr);
       }
-      account.save((err) => {
-        if(err) {
-          console.log(err);
+      account.save((saveErr) => {
+        if (saveErr) {
+          console.log(saveErr);
         } else {
-          req.login(account, (err) => {
-            if(err) {
-              console.log(err);
-            };
+          req.login(account, (loginErr) => {
+            if (loginErr) {
+              console.log(loginErr);
+            }
             res.json({
               username: req.user.username,
               id: req.user.id,
-            })
-          })
+            });
+          });
         }
       });
     });
+});
+
+// returns the home page html, index.html
+router.get('*', ensureAuthenticated, (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../public/index.html'));
+});
+
+router.get('/login', (req, res) => {
+  console.log('login page');
+  res.sendFile(path.resolve(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
