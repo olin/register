0af89ef27bd4c99e5d3f1e3f4c8a1a94aa5643b4
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-19 19:22:14 -0400

    Set up state persistence in store
    Still breaking when the page loads

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
 import { Provider } from 'react-redux';
-
+import { autoRehydrate, persistStore } from 'redux-persist';
 import App from './components/App';
 import reducer from './reducers/AccountPageReducer';
+
+const devTools = window.devToolsExtension ? window.devToolsExtension() : f => f;
 
 /* eslint-disable no-underscore-dangle */
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  compose(
+    devTools(),
+    autoRehydrate(),
+  ),
 );
 /* eslint-enable */
+
+persistStore(store);
 
 render(
   <Provider store={store}>
