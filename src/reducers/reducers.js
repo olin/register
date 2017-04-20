b0d68da9e3a217737bc2b9d587dd50b18bbe15b4
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-20 00:07:49 -0400

    Add reducer to handle redux-persist action

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import LoginReducer from './LoginReducer';
 import RegisterReducer from './RegisterReducer';
 import AccountPageReducer from './AccountPageReducer';
+import PersistReducer from './PersistReducer';
 
 const registerApp = combineReducers({
   AccountPageReducer,
   LoginReducer,
   RegisterReducer,
+  PersistReducer,
 });
 
 export default registerApp;
