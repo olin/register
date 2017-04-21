41f3c374e6aa8f8d9b7ced0024b8f87b7691fc44
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-21 14:23:36 -0400

    Create page to house semester stuff

---
+++ src/components/SemesterPlanPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SemesterList from './SemesterList';
+
+const SemesterPlanPage = () => (
+  <div>
+    <SemesterList />
+  </div>
+);
+
+export default SemesterPlanPage;
