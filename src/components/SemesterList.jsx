dbdcefc81d8883c2f6ddc2820fb922055e85ce07
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-21 14:24:06 -0400

    Add mapping to list Semester components

--- src/components/SemesterList.jsx
+++ src/components/SemesterList.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import SemesterContainer from '../containers/SemesterContainer'
+import SemesterContainer from '../containers/SemesterContainer';
 
 const SemesterList = () => (
-	<div>
-		<ul>
-			{for (let i = 0; i <= 8; i++) {
-				<SemesterContainer
-					id={'semester' + i.toString()}
-					num={i} // ownProps will grab this
-				/>
-			}}
-		</ul>
-	</div>
+  <div>
+    <ul>
+      {() => {
+        const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+        return nums.map(num =>
+          <SemesterContainer
+            key={'semester' + num.toString()}
+            num={num} // ownProps will grab this
+          />,
+        );
+      }}
+    </ul>
+  </div>
 );
 
 export default SemesterList;
