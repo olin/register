bd206fdb0006698ac352f85461e9d418de4c4af1
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-21 14:22:15 -0400

    Add link to semester page

--- src/components/NavPanel.jsx
+++ src/components/NavPanel.jsx
@@ -7,7 +7,7 @@ const NavPanel = ({ active }) => (
   <div className={styles.navpanel}>
     <Nav className={styles.pills} stacked activeKey={active} >
       <NavItem eventKey={1} className={styles.item} href="/">Home</NavItem>
-      <NavItem eventKey={2} className={styles.item} href="#">Create/Edit Plan of Study</NavItem>
+      <NavItem eventKey={2} className={styles.item} href="/semesterplan">Create/Edit Plan of Study</NavItem>
       <NavItem eventKey={3} className={styles.item} disabled>Other Stuff</NavItem>
     </Nav>
   </div>
