73cc5f0a8120022912261b1674b9c82d8431887f
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-25 15:55:09 -0400

    Change filterCourse to take both plannded and completed lists

--- src/containers/SemesterContainer.jsx
+++ src/containers/SemesterContainer.jsx
@@ -1,16 +1,24 @@
 import { connect } from 'react-redux';
 import Semester from '../components/Semester';
 
-const filterCourse = (num, courses) => (
-  courses.filter(course =>
-    course.semester === num,
-  )
-);
+const filterCourse = (num, completedCourses, plannedCourses) => {
+  const filteredCompleted = completedCourses.filter(course =>
+    course.courseStudent.semester === num,
+  );
+  const filteredPlanned = plannedCourses.filter(course =>
+    course.courseStudent.semester === num,
+  );
+  return filteredPlanned.concat(filteredCompleted);
+};
 
 const mapStateToProps = (state, ownProps) => (
   {
     num: ownProps.num,
-    courseList: filterCourse(ownProps.num, state.LoginReducer.completedCourses),
+    courseList: filterCourse(
+      ownProps.num,
+      state.LoginReducer.completedCourses,
+      state.LoginReducer.plannedCourses,
+    ),
   }
 );
 
