d600081702904135f74a52f9fb91c579e1a5fabb
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-25 15:52:37 -0400

    Experiment with how to load course info

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const passport = require('passport');
 const Student = require('./../models/studentModel');
+const Course = require('../models/courseModel');
 const path = require('path');
 
 const router = express.Router();
 
+const courseInfo = (courseList) => {
+  const augmentedCourses = [];
+  for (let i = 0; i < courseList.length; i += 1) {
+    Course.findOne({ _id: courseList[i].courseId }, (err, course) => {
+      if (err) {
+        console.error(err);
+      } else {
+        augmentedCourses.push({
+          courseInfo: course,
+          courseStudent: courseList[i],
+        });
+      }
+    });
+  }
+};
 // log in with local strategy
 router.post('/login', passport.authenticate('local'),
   (req, res) => {
-    res.json(req.user);
+    const userObject = req.user;
+    userObject.completedCourses = courseInfo(req.user.completedCourses);
+    userObject.plannedCourses = courseInfo(req.user.plannedCourses);
+    res.json(userObject);
   });
 
 // register new user
@@ -26,7 +45,10 @@ router.post('/register', (req, res) => {
             if (loginErr) {
               console.log(loginErr);
             }
-            res.json(req.user);
+            const userObject = req.user;
+            userObject.completedCourses = courseInfo(req.user.completedCourses);
+            userObject.plannedCourses = courseInfo(req.user.plannedCourses);
+            res.json(userObject);
           });
         }
       });
