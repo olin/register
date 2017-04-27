84b8ce203440eba61aec0a0e86607c3f05b7b2c0
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-26 23:52:26 -0400

    Match structure to Semester component's and remove unused props

--- src/components/SemesterReserve.jsx
+++ src/components/SemesterReserve.jsx
@@ -1,39 +1,30 @@
-import REact, { PropTypes } from 'react';
-import CourseBlock from './CourseBLock';
+import React, { PropTypes } from 'react';
+import CourseBlockContainer from '../containers/CourseBlockContainer'; // this will be changed to a container later!
 
-const SemesterReserve = ({ courseList }) => (
+const Semester = ({ courseList }) => (
   <div>
-    Assign to a semester:
+    Unassigned Courses:
     <li>
       <ul>
         {courseList.map(course =>
-          <CourseBlock
+          <CourseBlockContainer
             key={course.id}
-            name={course.name}
-            completed={course.grade && true}
+            course={course}
           />,
         )}
       </ul>
+      <hr />
     </li>
   </div>
 );
 
+
 Semester.propTypes = {
   courseList: PropTypes.arrayOf(PropTypes.shape({
-    courseInfo: PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      registrarId: PropTypes.string.isRequired,
-      title: PropTypes.string.isRequired,
-      credits: PropTypes.number.isRequired,
-      ENGRCredits: PropTypes.number,
-      AHSCredits: PropTypes.number,
-      MTHCredits: PropTypes.number,
-      SCICredits: PropTypes.number,
-      ECredits: PropTypes.number,
-      NDGRCredits: PropTypes.number,
-    }).isRequired,
-    courseStudent: PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      courseId: PropTypes.string.isRequired,
+    courseId: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    grade: PropTypes.string,
   }).isRequired).isRequired,
-}
+};
+
+export default Semester;
