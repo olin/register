acbad37f3981d3200297bbb4ed7f4727c9316f8d
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-30 12:37:24 -0400

    Incorporate drag and drop props into component

--- src/components/CourseBlock.jsx
+++ src/components/CourseBlock.jsx
@@ -1,15 +1,30 @@
 import React, { PropTypes } from 'react';
 
-const CourseBlock = ({ title, registrarId, completed }) => (
-  <li>
-    {registrarId}:{title} {completed.toString()}
-  </li>
-);
+const CourseBlock = ({
+  title,
+  registrarId,
+  completed,
+  connectDragSource,
+  isDragging,
+}) => {
+  const textColor = completed ? 'red' : 'black';
+  return connectDragSource(
+    <li
+      style={{
+        color: textColor,
+      }}
+    >
+      {registrarId}:{title}
+    </li>,
+  );
+};
 
 CourseBlock.propTypes = {
   title: PropTypes.string.isRequired,
   registrarId: PropTypes.string.isRequired,
   completed: PropTypes.bool.isRequired, // for styling?
+  connectDragSource: PropTypes.func.isRequired,
+  isDragging: PropTypes.bool.isRequired,
 };
 
 export default CourseBlock;
