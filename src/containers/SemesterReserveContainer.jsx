8c1a473ab1f1b7a23a4653f70852a01179ffd616
arianaolson419 <ariana.olson@students.olin.edu>
2017-04-30 15:18:40 -0400

    Convert to drop target

--- src/containers/SemesterReserveContainer.jsx
+++ src/containers/SemesterReserveContainer.jsx
@@ -1,22 +1,47 @@
-import { connect } from 'react-redux';
+import { connect as connectRedux } from 'react-redux';
+import { DropTarget } from 'react-dnd';
 import SemesterReserve from '../components/SemesterReserve';
 
 const filterCourse = (plannedCourses) => {
+  const compare = (a, b) => {
+    if (a.registrarId.toUpperCase() < b.registrarId.toUpperCase()) {
+      return -1;
+    }
+    return 1;
+  };
   const filteredPlanned = plannedCourses.filter(course =>
     course.semester === undefined,
-  );
+  ).sort(compare);
   return filteredPlanned;
 };
 
-const mapStateToProps = (state, ownProps) => (
+const semesterTarget = {
+  drop(props, monitor) {
+    const sourceCourse = monitor.getItem();
+    return {
+      courseId: sourceCourse.course.courseId,
+      newSemester: undefined,
+    };
+  },
+  canDrop(props, monitor) {
+    const sourceCourse = monitor.getItem();
+    return sourceCourse.course.semester !== undefined;
+  },
+};
+
+const collect = connect => ({
+  connectDropTarget: connect.dropTarget(),
+});
+
+const mapStateToProps = state => (
   {
-    key: ownProps.semester,
     courseList: filterCourse(state.LoginReducer.plannedCourses),
   }
 );
 
-const SemesterReserveContainer = connect(
+const Wrapper = DropTarget('course', semesterTarget, collect)(SemesterReserve);
+const SemesterReserveContainer = connectRedux(
   mapStateToProps,
-)(SemesterReserve);
+)(Wrapper);
 
 export default SemesterReserveContainer;
