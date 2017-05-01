2d9d39bc5868e298ef37668b7e85f7356ca35f57
arianaolson419 <ariana.olson@students.olin.edu>
2017-05-01 18:19:11 -0400

    Import new reducers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,12 +1,13 @@
 import { combineReducers } from 'redux';
 import ProgressTrackerReducer from './ProgressTrackerReducer';
-import LoginReducer from './LoginReducer';
-import RegisterReducer from './RegisterReducer';
-import SettingsPageReducer from './SettingsPageReducer';
+import StudentReducer from './StudentReducer';
+import CourseReducer from './CourseReducer';
 import CoursePlannerReducer from './CoursePlannerReducer';
 import PersistReducer from './PersistReducer';
 import ReqsReducer from './ReqsReducer';
 
+
+console.log(SettingsPageReducer);
 const registerApp = combineReducers({
   SettingsPageReducer,
   CoursePlannerReducer,
