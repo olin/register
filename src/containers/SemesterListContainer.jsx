78282777975945f25bf4373572f6ee3c2a055636
rsiegel812 <robert.siegel@students.olin.edu>
2017-04-30 22:39:37 -0400

    Added Button to Save Course Plan

--- src/containers/SemesterListContainer.jsx
+++ src/containers/SemesterListContainer.jsx
@@ -17,6 +17,10 @@ const makeSemesterList = (entryYear) => {
 
 const mapStateToProps = state => ({
   semesters: makeSemesterList(state.Student.entryYear),
+  onSavePlan: (e) => {
+    e.preventDefault();
+    // dispatch()
+  },
 });
 
 const SemesterListContainer = connect(
