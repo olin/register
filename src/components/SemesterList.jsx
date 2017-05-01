53654016cd4143560a28f9524879034d744065fe
rsiegel812 <robert.siegel@students.olin.edu>
2017-04-30 22:39:37 -0400

    Added Button to Save Course Plan

--- src/components/SemesterList.jsx
+++ src/components/SemesterList.jsx
@@ -3,7 +3,7 @@ import React, { PropTypes } from 'react';
 import SemesterContainer from '../containers/SemesterContainer';
 import SemesterReserveContainer from '../containers/SemesterReserveContainer';
 
-const SemesterList = ({ semesters }) => (
+const SemesterList = ({ semesters, onSavePlan }) => (
   <div>
     <h1>Plan by Semester</h1>
     <ul>
@@ -16,11 +16,17 @@ const SemesterList = ({ semesters }) => (
       )}
       <SemesterReserveContainer />
     </ul>
+    <form onSubmit={onSavePlan}>
+      <button type="submit" value="UpdatePlan">
+        Save Plan of Study
+      </button>
+    </form>
   </div>
 );
 
 SemesterList.propTypes = {
   semesters: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onSavePlan: PropTypes.func.isRequired,
 };
 
 export default SemesterList;
