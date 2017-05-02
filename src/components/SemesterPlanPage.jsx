4c8c01d3dc0865305b598da85344292acad70344
rsiegel812 <robert.siegel@students.olin.edu>
2017-05-02 02:36:59 -0400

    Added NavBar to SemesterPlanPage

--- src/components/SemesterPlanPage.jsx
+++ src/components/SemesterPlanPage.jsx
@@ -1,11 +1,22 @@
 // The page that holds the semester list.
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 import SemesterListContainer from '../containers/SemesterListContainer';
+import NavPanel from './NavPanel';
+import styles from '../../public/stylesheets/pages.css';
 
 const SemesterPlanPage = () => (
-  <div>
-    <SemesterListContainer />
-  </div>
+  <Row>
+    {/* col attributes resize/reorder for different size screens */}
+    <Col sm={3} lg={2}>
+      <NavPanel />
+    </Col>
+    <Col sm={9} lg={10}>
+      <ul className={styles.mainbody}>
+        <SemesterListContainer />
+      </ul>
+    </Col>
+  </Row>
 );
 
 export default SemesterPlanPage;
