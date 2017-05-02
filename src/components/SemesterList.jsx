320ec9cdf6258dbf75f58189d035e83174a03bcb
arianaolson419 <ariana.olson@students.olin.edu>
2017-05-02 02:05:05 -0400

    Remove button from component

--- src/components/SemesterList.jsx
+++ src/components/SemesterList.jsx
@@ -15,11 +15,6 @@ const SemesterList = ({ semesters, onSavePlan }) => (
       )}
       <SemesterReserveContainer />
     </ul>
-    <form onSubmit={onSavePlan}>
-      <button type="submit" value="UpdatePlan">
-        Save Plan of Study
-      </button>
-    </form>
   </div>
 );
 
