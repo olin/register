c1cb6fd5966ff83dac4f6f9cd16fa363ec5c6f3c
Logan Davis <logankeahidavis@gmail.com>
2017-05-02 14:28:20 -0400

    Add error bar for registration errors, with styling

--- src/components/RegisterNewUser.jsx
+++ src/components/RegisterNewUser.jsx
@@ -1,12 +1,13 @@
 /* The form that a new user fills out to register a new account.
 The user provides their name, an email, and a password. */
 import React, { PropTypes } from 'react';
+import { Alert, Glyphicon } from 'react-bootstrap';
 import styles from './../../public/stylesheets/login.css';
-import Note from './Note';
 
 const RegisterNewUser = ({
   username,
   password,
+  registerError,
   updateRegisterUser,
   updateRegisterPwd,
   onRegister,
@@ -14,7 +15,14 @@ const RegisterNewUser = ({
   <div className={styles.registerblock}>
     <form id="register" className="form-signin" onSubmit={onRegister}>
       <h2>Register a New Account</h2>
-      {/* registerError ? <Note text={registerError} /> : null */}
+      { registerError ?
+        <div>
+          <Alert className={styles.alert} bsStyle="danger">
+            <Glyphicon glyph="exclamation-sign" /> {registerError}!
+          </Alert>
+          <br />
+        </div>
+        : null }
       <input
         placeholder="Email"
         type="text"
@@ -40,6 +48,7 @@ const RegisterNewUser = ({
 RegisterNewUser.propTypes = {
   username: PropTypes.string.isRequired,
   password: PropTypes.string.isRequired,
+  registerError: PropTypes.string.isRequired,
   updateRegisterUser: PropTypes.func.isRequired,
   updateRegisterPwd: PropTypes.func.isRequired,
   onRegister: PropTypes.func.isRequired,
