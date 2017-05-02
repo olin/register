306aa344f088c69b3edcaab2943e9a098ec85bd0
Logan Davis <logankeahidavis@gmail.com>
2017-05-02 02:29:19 -0400

    Touch up style on login page

--- src/components/LoginLocal.jsx
+++ src/components/LoginLocal.jsx
@@ -1,10 +1,10 @@
 import React, { PropTypes } from 'react';
-import styles from './../../public/stylesheets/login-local.css';
+import styles from './../../public/stylesheets/login.css';
 
 const LoginLocal = ({ username, password, updateUser, updatePassword, onLogin }) => (
   <div className={styles.loginblock}>
     <form id="local-login" className="form-signin" onSubmit={onLogin}>
-      <h2>Login with email and password</h2>
+      <h2>Log In to an Existing Account</h2>
       <input
         placeholder="Email"
         type="text"
